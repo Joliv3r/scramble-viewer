a631b0a973ab6f8aecaebd7719861bb2220421d4
Joliver <jacobbruun@hotmail.no>
2025-06-28 17:13:00 +0200

    feat: remove "Cube" from schedule

--- sort-events.sh
+++ sort-events.sh
@@ -7,8 +7,8 @@ if [ $# -eq 0 ]
 fi
 
 JSON_FILE="$1-json"
-SCHEDULE_FILE="$1-schedule"
+SCHEDULE_FILE="schedule"
 wget --output-document $JSON_FILE https://worldcubeassociation.org/api/v0/competitions/$1/wcif/public
-cat $JSON_FILE | jq --from-file query.jq | tail -n +3 | head -n -2 | sed -r 's/[^[:alnum:][:space:]]//g' | sed -r 's/    //g' | tee $SCHEDULE_FILE
+cat $JSON_FILE | jq --from-file query.jq | tail -n +3 | head -n -2 | sed -r 's/[^[:alnum:][:space:]]//g' | sed -r 's/    //g' | sed -r 's/ Cube//g' | tee $SCHEDULE_FILE
 
 
