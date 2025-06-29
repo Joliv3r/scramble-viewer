cbc0092391ee996a49619a961ab087a398c779bf
Joliver <jacobbruun@hotmail.no>
2025-06-29 16:37:32 +0200

    feat: remove empty lines

--- sort-events.sh
+++ sort-events.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if [ $# -eq 0 ]
   then
     echo "I need a competition ID..."
@@ -10,5 +8,3 @@ JSON_FILE="$1-json"
 SCHEDULE_FILE="schedule"
 wget --output-document $JSON_FILE https://worldcubeassociation.org/api/v0/competitions/$1/wcif/public
 cat $JSON_FILE | jq --from-file query.jq | tail -n +3 | head -n -2 | sed -r 's/[^[:alnum:][:space:]]//g' | sed -r 's/    //g' | sed -r 's/ Cube//g' | tee $SCHEDULE_FILE
-
-
