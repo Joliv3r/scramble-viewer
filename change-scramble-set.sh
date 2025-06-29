bd95165928c16d586394ddf8ee12abb7d063f1c7
Joliver <jacobbruun@hotmail.no>
2025-06-29 14:19:09 +0200

    feat: handling non valid scramble sets

--- change-scramble-set.sh
+++ change-scramble-set.sh
@@ -5,12 +5,18 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+SCRAMBLE_DIR="./scrambles/"
 SCRAMBLE_SET_TO_CHANGE=$1
+
+if [ ! -f "$SCRAMBLE_DIR$SCRAMBLE_SET_TO_CHANGE" ]; then
+  echo "$SCRAMBLE_SET_TO_CHANGE is not a valid scramble set..."
+  exit 1
+fi
 
 echo -n "Do you want to display $SCRAMBLE_SET_TO_CHANGE? [y/N] " 
 read answer
 
 case $answer in 
-  y|Y) cp "./scrambles/$SCRAMBLE_SET_TO_CHANGE" ./scrambles/currently-active.pdf ;;
+  y|Y) cp "$SCRAMBLE_DIR$SCRAMBLE_SET_TO_CHANGE" ./scrambles/currently-active.pdf ;;
   *) echo "Nothing was done" ;;
 esac
